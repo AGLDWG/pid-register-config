60896f3283ca81dd32b1c82bf2f21b398786fb95
Nicholas Car <nick@kurrawong.ai>
2026-09-08 21:23:54 +1000

    simple content overrides for index and about pages

--- prez-ui/overrides/app/pages/index.vue
+++ prez-ui/overrides/app/pages/index.vue
@@ -2,6 +2,26 @@
     <NuxtLayout contentonly>
         <template #default>
             <h1 class="text-2xl mt-8">PID Register</h1>
+            <div class="home-copy">
+                <p>
+                    This is the register of the
+                    <a href="https://www.linked.data.gov.au">Australian Government Linked Data Working Group</a>'s
+                    Persistent Identifiers (PIDs).
+                </p>
+                <p>
+                    These PIDs are allocated for Australian government
+                    <a href="https://en.wikipedia.org/wiki/Linked_data">Linked Data</a> objects, which can be datasets or
+                    definitional items (vocabularies &amp; ontologies).
+                </p>
+                <p>
+                    In addition to the primary PID register, secondary registers of the models, validators and
+                    organisations that support the PIDs are kept here too.
+                </p>
+                <p>
+                    For more information about PID allocation, see the
+                    <a href="https://www.linked.data.gov.au/governance">AGLDWG's Governance page</a>.
+                </p>
+            </div>
         </template>
     </NuxtLayout>
 </template>
